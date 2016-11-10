eb3f7b33bbd4c39a4141588c547aa283860c9b2b
hieplq <lequanghiep74@gmail.com>
2016-11-10 20:04:11 +0700

    change name

--- src/net/z3testgen/GenTest.java
+++ src/net/z3testgen/GenTest.java
@@ -8,7 +8,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
 
-public class TestGenTriangleType {
+public class GenTest {
 
     @SuppressWarnings("null")
     public static void main(String[] args) throws Z3Exception, IOException {
